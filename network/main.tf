a9586701a3d0d49a02fe381bd3036630a4366124
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-07 17:38:00 +0100

    Update main.tf

--- network/main.tf
+++ network/main.tf
@@ -1,1 +1,20 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.virtual_network_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = [var.address_space]
+}
 
+resource "azurerm_subnet" "pesubnet" {
+  name                 = var.pesubnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.pesubnet_prefix]
+}
+
+resource "azurerm_subnet" "acasubnet" {
+  name                 = var.acasubnet_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.acasubnet_prefix]
+}
