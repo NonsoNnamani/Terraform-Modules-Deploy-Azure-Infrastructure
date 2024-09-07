07e7d865d477496e6c06afdcd45a10a2221bed53
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-07 17:40:35 +0100

    Update main.tf

--- container_registry/main.tf
+++ container_registry/main.tf
@@ -1,1 +1,7 @@
-
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.container_registry_sku
+  admin_enabled       = var.admin_enabled
+}
