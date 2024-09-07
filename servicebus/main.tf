b54ebb023ab854c4881755a8d8627db3dbd7ba10
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-07 17:39:09 +0100

    Update main.tf

--- servicebus/main.tf
+++ servicebus/main.tf
@@ -1,1 +1,6 @@
-
+resource "azurerm_servicebus_namespace" "sb_namespace" {
+  name                = var.servicebus_namespace_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.servicebus_sku
+}
