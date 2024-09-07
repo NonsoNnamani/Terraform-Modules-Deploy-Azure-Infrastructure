de19ae87eed5e895e56d0c475c9498217ce9e680
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-07 17:38:23 +0100

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -1,1 +1,31 @@
+variable "virtual_network_name" {
+  description = "The name of the virtual network"
+}
 
+variable "location" {
+  description = "The location of the resources"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "address_space" {
+  description = "The address space for the virtual network"
+}
+
+variable "pesubnet_name" {
+  description = "The name of the PE subnet"
+}
+
+variable "pesubnet_prefix" {
+  description = "The address prefix for the PE subnet"
+}
+
+variable "acasubnet_name" {
+  description = "The name of the ACA subnet"
+}
+
+variable "acasubnet_prefix" {
+  description = "The address prefix for the ACA subnet"
+}
