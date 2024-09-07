f6e8f5c8c963a82999595020105caed64306be00
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-08 00:51:51 +0100

    Update main.tf - added Terraform remote backend

--- main.tf
+++ main.tf
@@ -35,3 +35,31 @@ module "container_registry" {
   container_registry_sku  = var.container_registry_sku
   admin_enabled           = var.admin_enabled
 }
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "RG1"
+    storage_account_name  = azurerm_storage_account.tfstate.name
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+  }
+}
