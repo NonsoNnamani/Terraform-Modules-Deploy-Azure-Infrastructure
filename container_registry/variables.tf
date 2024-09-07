fd0b5545a2006a0be57d4f7bbcc5114aa49c6d64
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-07 17:40:59 +0100

    Update variables.tf

--- container_registry/variables.tf
+++ container_registry/variables.tf
@@ -1,1 +1,20 @@
+variable "container_registry_name" {
+  description = "The name of the Container Registry"
+}
 
+variable "location" {
+  description = "The location of the resources"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "container_registry_sku" {
+  description = "The SKU for the Container Registry"
+}
+
+variable "admin_enabled" {
+  description = "Enable admin user for the Container Registry"
+  type        = bool
+}
