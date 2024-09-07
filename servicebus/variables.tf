57fc5b18b733dbf19822c35adfc59fef6a204840
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-07 17:39:59 +0100

    Update variables.tf

--- servicebus/variables.tf
+++ servicebus/variables.tf
@@ -1,1 +1,15 @@
+variable "servicebus_namespace_name" {
+  description = "The name of the Service Bus namespace"
+}
 
+variable "location" {
+  description = "The location of the resources"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "servicebus_sku" {
+  description = "The SKU for the Service Bus namespace"
+}
