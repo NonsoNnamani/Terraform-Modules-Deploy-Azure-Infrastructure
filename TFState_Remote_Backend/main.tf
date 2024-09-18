128ea4acd01182da21d568dc815c60dd77299fc7
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-18 01:42:18 +0100

    Create main.tf

---
+++ TFState_Remote_Backend/main.tf
@@ -0,0 +1,27 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "tfstate${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
