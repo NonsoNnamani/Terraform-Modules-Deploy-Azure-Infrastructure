03332bb8e9510a702cab8ba6794aabada643d814
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-18 01:54:34 +0100

    Update main.tf

--- Resource_Deployment/main.tf
+++ Resource_Deployment/main.tf
@@ -36,25 +36,6 @@ module "container_registry" {
   admin_enabled           = var.admin_enabled
 }
 
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "private"
-}
-
 terraform {
   backend "azurerm" {
     resource_group_name   = "RG1"
