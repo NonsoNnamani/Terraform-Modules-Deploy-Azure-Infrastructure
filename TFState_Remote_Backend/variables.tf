d6a4095717da89deca1dd17ae97fa4190a00f930
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-25 18:40:56 +0100

    Update variables.tf

--- TFState_Remote_Backend/variables.tf
+++ TFState_Remote_Backend/variables.tf
@@ -2,3 +2,28 @@ variable "resource_group_name" {
   description = "The name of the resource group"
   default     = "BackendRG"
 }
+
+variable "location" {
+  description = "location where storage account will be created"
+  default = "centralus"
+}
+
+variable "account_tier" {
+  description = "the storage account tier"
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  description = "replication type for the storage account"
+  default = "LRS"
+}
+
+variable "azurerm_storage_container_name" {
+  description = "name of the container in the azure storage account"
+  default = "tfstate"
+}
+
+variable "container_access_type" {
+  description = "access type for the storage container"
+  default = "private"
+}
