72197fed76f4e962cac94642888428fba08fb046
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-26 13:45:08 +0100

    Update update_terraform_backend_details.ps1

--- TFState_Remote_Backend/update_terraform_backend_details.ps1
+++ TFState_Remote_Backend/update_terraform_backend_details.ps1
@@ -5,6 +5,10 @@ $env:TF_VAR_tfstate_remote_backend_storage_container_name = terraform output -ra
 
 # Export sensitive outputs
 # Optionally, print the exported variables (be careful with sensitive data)
+Write-Host " "
 Write-Host "Exported tfstate_remote_backend_rg_name: $env:TF_VAR_tfstate_remote_backend_rg_name"
 Write-Host "Exported tfstate_remote_backend_storage_account_name: $env:TF_VAR_tfstate_remote_backend_storage_account_name"
 Write-Host "Exported tfstate_remote_backend_storage_container_name: $env:TF_VAR_tfstate_remote_backend_storage_container_name"
+
+Write-Host "To use the above in your terraform file, run the following code :"
+Write-Host 'terraform init -backend-config="backend_config.tfvars"'
