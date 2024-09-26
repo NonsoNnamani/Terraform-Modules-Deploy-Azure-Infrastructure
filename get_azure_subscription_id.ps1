62fe6fe81d553eeea572f2c0fd48a617fa0ce635
Chukwunonso Nnamani <104857287+NonsoNnamani@users.noreply.github.com>
2024-09-26 13:31:47 +0100

    Update get_azure_subscription_id.ps1

--- get_azure_subscription_id.ps1
+++ get_azure_subscription_id.ps1
@@ -3,3 +3,7 @@ $subscriptionid = $subscription | where-object {$PSItem.State -Like "*Enabled*"}
 
 # Select the first account with state enabled and save to Terraform environment variable
 $Env:TF_VAR_azure_subscription_id = $subscriptionid[0].id
+
+write-host " "
+write-host "Azure Subscription id has been saved to environment variables (TF_VAR_azure_subscription_id) = $Env:TF_VAR_azure_subscription_id "
+write-host " "
